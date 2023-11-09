631e5ddc60aaba375aa18758533c0655ff67d6a0
w9610281 <w9610281@live.tees.ac.uk>
2023-11-09 17:54:51 +0000

    Started writing a UI

--- app/src/main/java/uk/ac/tees/scedt/mad/w9610281/milkit/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/scedt/mad/w9610281/milkit/MainActivity.kt
@@ -3,44 +3,80 @@ package uk.ac.tees.scedt.mad.w9610281.milkit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import uk.ac.tees.scedt.mad.w9610281.milkit.ui.theme.MilkitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MilkitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Main()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Main() {
+    var name: String = "default"
+    var pw: String = "*****"
+    Column {
+        Login(name = name)
+        Password(pw = pw)
+        Row {
+            SignIn()
+            Signup()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login(name: String) {
+    OutlinedTextField(
+        value = name,
+        onValueChange = {},
+        label = { Text(text = "Name")}
     )
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Password(pw: String) {
+    OutlinedTextField(
+        value = pw,
+        onValueChange = {},
+        label = { Text(text = "Password")}
+    )
+}
+
+@Composable
+fun Signup() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Sign up")
+    }
+}
+
+@Composable
+fun SignIn() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Sign in")
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MilkitTheme {
-        Greeting("Android")
-    }
+    Main()
+//    MilkitTheme {
+//        Greeting("Android")
+//    }
 }
